c403b9a5219abaeae88dbfb1ce2e3245d10f5f4c
José Mauro Neto <jmauroneto@gmail.com>
2020-03-30 14:04:48 -0300

    Finalizando desafios

--- semana3/logica-booleana/index.js
+++ semana3/logica-booleana/index.js
@@ -108,13 +108,51 @@ console.log("Resposta: " + email)
 
 console.log("Questão 3:")
 let consumo = prompt("Qual o seu consumo mensal em kwh? (apenas números)")
-let porcentagem = prompt("Qual a porcentagem de desconto?")
+let porcentagem = prompt("Qual a porcentagem de desconto? (apenas numeros. Ex: se 15%, colocar apenas 15")
 consumo *= 0.05;
 console.log("Valor a ser pago sem desconto: R$" + consumo.toFixed(2))
 consumo = consumo - (consumo*(porcentagem/100));
 console.log("Valor a ser pago com desconto: R$" +consumo.toFixed(2));
 
+
+console.log("Consumo de 280kWh e 15% de desconto")
 consumo = 280 * 0.05;
 console.log("a. Valor a ser pago sem desconto: R$" + consumo.toFixed(2))
 consumo = consumo - (consumo*(15/100));
 console.log("Valor a ser pago com desconto: R$" +consumo.toFixed(2));
+
+
+
+console.log("Desafio:")
+let lbParaKg = 20*0.453592
+console.log("a. 20lb equivalem a "+lbParaKg+"kg");
+let ozParaKg = 10*0.0283495
+console.log("b. 10oz equivalem a "+ozParaKg.toFixed(5)+"kg");
+let miParaM = 100*1609.34
+console.log("c. 100mi equivalem a "+miParaM+"m");
+let ftParaM = 50*0.3048
+console.log("d. 50ft equivalem a "+ftParaM+"m");
+let galParaL = 103.56*3.78541
+console.log("e. 103,56gal equivalem a "+galParaL.toFixed(3)+"l");
+let xicParaL = 450*0.24
+console.log("f. 450xic equivalem a "+xicParaL+"l");
+
+console.log("g.")
+let lb = prompt("Digite o valor em lb para converter para kg")
+lbParaKg = lb*0.453592
+console.log("I. "+lb+"lb equivalem a "+lbParaKg+"kg");
+let oz = prompt("Digite o valor em oz para converter para kg")
+ozParaKg = oz*0.0283495
+console.log("II. "+ oz +"oz equivalem a "+ozParaKg.toFixed(5)+"kg");
+let mi = prompt("Digite o valor em mi para converter para m")
+miParaM = mi*1609.34
+console.log("III. "+mi+"mi equivalem a "+miParaM+"m");
+let ft = prompt("Digite o valor em ft para converter para m")
+ftParaM = ft*0.3048
+console.log("IV. "+ft+"ft equivalem a "+ftParaM+"m");
+let gal = prompt("Digite o valor em gal para converter para l")
+galParaL = gal*3.78541
+console.log("V. "+gal+"gal equivalem a "+galParaL.toFixed(3)+"l");
+let xic = prompt("Digite o valor em xic para converter para kg")
+xicParaL = xic*0.24
+console.log("VI. "+xic+"xic equivalem a "+xicParaL+"l");
