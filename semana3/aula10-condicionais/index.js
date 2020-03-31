1a64612ba511afd1fe8befb394b26789cb78bd41
José Mauro Neto <jmauroneto@gmail.com>
2020-03-31 17:17:31 -0300

    Arrumando algoritmo de ordenação

--- semana3/aula10-condicionais/index.js
+++ semana3/aula10-condicionais/index.js
@@ -47,22 +47,23 @@ num1 = Number(prompt("Digite o primeiro número"))
 num2 = Number(prompt("Digite o segundo número"))
 let num3 = Number(prompt("Digite o terceiro número"))
 
-
-if(num1 < num3){
-    temp=num1
-    num1=num3
-    num3=temp
-}
-if(num1 < num2){
-    temp=num1
-    num1=num2
+if(num3 > num2){
+    temp=num3
+    num3=num2
     num2=temp
 }
-if(num2 < num3){
+if(num2 > num1){
     temp=num2
-    num2=num3
-    num3=temp
+    num2=num1
+    num1=temp
 }
+if(num3 < num2){
+    temp=num3
+    num3=num2
+    num2=temp
+}
+
+
 
 console.log(num1, num2,num3)
 
