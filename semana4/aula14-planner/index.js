cb6c2d17c722f47f57c41d9160640fe06f2d73df
José Mauro Neto <jmauroneto@gmail.com>
2020-04-07 16:22:22 -0300

    Adicionando função de riscar

--- semana4/aula14-planner/index.js
+++ semana4/aula14-planner/index.js
@@ -4,25 +4,25 @@ function adicionarTarefa(){
     if(atividade !== ""){
         switch(dia){
             case "segunda":
-                document.getElementById("segunda").innerHTML += "<li>"+atividade+"</li>"
+                document.getElementById("segunda").innerHTML += "<li onclick='riscar(this)'>"+atividade+"</li>"
                 break;
             case "terca":
-                document.getElementById("terca").innerHTML += "<li>"+atividade+"</li>"
+                document.getElementById("terca").innerHTML += "<li onclick='riscar(this)'>"+atividade+"</li>"
                 break;
             case "quarta":
-                document.getElementById("quarta").innerHTML += "<li>"+atividade+"</li>"
+                document.getElementById("quarta").innerHTML += "<li onclick='riscar(this)'>"+atividade+"</li>"
                 break;
             case "quinta":
-                document.getElementById("quinta").innerHTML += "<li>"+atividade+"</li>"
+                document.getElementById("quinta").innerHTML += "<li onclick='riscar(this)'>"+atividade+"</li>"
                 break;
             case "sexta":
-                document.getElementById("sexta").innerHTML += "<li>"+atividade+"</li>"
+                document.getElementById("sexta").innerHTML += "<li onclick='riscar(this)'>"+atividade+"</li>"
                 break;
             case "sabado":
-                document.getElementById("sabado").innerHTML += "<li>"+atividade+"</li>"
+                document.getElementById("sabado").innerHTML += "<li onclick='riscar(this)'>"+atividade+"</li>"
                 break;
             case "domingo":
-                document.getElementById("domingo").innerHTML += "<li>"+atividade+"</li>"
+                document.getElementById("domingo").innerHTML += "<li onclick='riscar(this)'>"+atividade+"</li>"
                 break;
         }
         document.getElementById("atividade").value="";
@@ -30,6 +30,13 @@ function adicionarTarefa(){
     else{
         alert("A tarefa não pode ser vazia!")
     }
-    const teste =  document.getElementsByTagName("li");
-        console.log(teste)
+}
+
+function riscar(risca){
+    if(risca.style.textDecoration === "line-through"){
+        risca.style.textDecoration="";
+    }
+    else{
+        risca.style.textDecoration="line-through";
+    }
 }
