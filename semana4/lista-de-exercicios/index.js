86151809a4513f2ec79f74e9cbf002316dd77e8b
José Mauro Neto <jmauroneto@gmail.com>
2020-04-07 14:42:49 -0300

    Finalizando exercício 1 de escrita de código

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -47,5 +47,42 @@ Em seguida é feita outra comparação, comparando se numero é maior que numero
 numero2 recebe o valor de numero.
 Ao sair do laço são printados no console o numero1 e numero2, que serão o menor e o maior número da array,
 respectivamente, nesse caso, -10 e 1590.
+
 */
 
+//Exercícios de escrita de código
+
+/*
+Exercício 1
+booleano1 = true
+booleano2 = false
+booleano3 = !booleano2 = true
+booleano 4 = !booleano3 = false
+
+a) true && false && false 
+false
+b) (true && false) || !true
+false || false
+false
+c)(false || true) && (false || true)
+true && true
+true
+d)!(false && true) || !(true && true)
+!false || !true
+true || false
+true
+e)!(true) && !(true) || (!false && true && true)
+false && false || (true && true && true)
+false || true
+true
+
+
+Respostas:
+a - false
+b - false
+c - true
+d - true
+e - true
+*/
+
+//Exercício 2
