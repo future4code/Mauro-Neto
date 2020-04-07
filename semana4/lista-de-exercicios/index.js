0ea20882463324ac2207ce8dc42e7569ea141f34
José Mauro Neto <jmauroneto@gmail.com>
2020-04-07 14:13:51 -0300

    Exercício 1 finalizado

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,16 @@
+/* Exercícios de leitura de código
+
+Exercício 1 
+Função:
+A função recebe um parametro de valorEmDolar. Dentro da função é criada uma variável
+que recebe do usuário um numeral informando a cotação do dolar. A função retorna a multiplicação do
+valorEmDolar(passado no início da função) multiplicada pela cotação definida pelo usuário.
+Global:
+É declarada uma variável que chama a função passando 100 como parâmetro. O esperado é que
+apareça um prompt para digitar o valor da cotação atual. Após receber a cotação, faz-se o cálculo e 
+a variável meuDinheiro recebe o valor retornado da função.
+Em seguida, deve-se printar no console o valor da variável meuDinheiro.
+
+
+*/
+
