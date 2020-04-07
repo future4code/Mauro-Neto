f770ba969a6624301f06e6bd38d64bbce33ff131
José Mauro Neto <jmauroneto@gmail.com>
2020-04-07 18:03:29 -0300

    Faltando responsividade

--- semana4/aula14-planner/index.js
+++ semana4/aula14-planner/index.js
@@ -1,30 +1,11 @@
 function adicionarTarefa(){
     const atividade = document.getElementById("atividade").value;
     const dia = document.getElementById("dia").value;
+    const hora = document.getElementById("hora").value;
+    const diaEHora = dia+hora;
+    console.log(diaEHora);
     if(atividade !== ""){
-        switch(dia){
-            case "segunda":
-                document.getElementById("segunda").innerHTML += "<li onclick='riscar(this)'>"+atividade+"</li>"
-                break;
-            case "terca":
-                document.getElementById("terca").innerHTML += "<li onclick='riscar(this)'>"+atividade+"</li>"
-                break;
-            case "quarta":
-                document.getElementById("quarta").innerHTML += "<li onclick='riscar(this)'>"+atividade+"</li>"
-                break;
-            case "quinta":
-                document.getElementById("quinta").innerHTML += "<li onclick='riscar(this)'>"+atividade+"</li>"
-                break;
-            case "sexta":
-                document.getElementById("sexta").innerHTML += "<li onclick='riscar(this)'>"+atividade+"</li>"
-                break;
-            case "sabado":
-                document.getElementById("sabado").innerHTML += "<li onclick='riscar(this)'>"+atividade+"</li>"
-                break;
-            case "domingo":
-                document.getElementById("domingo").innerHTML += "<li onclick='riscar(this)'>"+atividade+"</li>"
-                break;
-        }
+        document.getElementById(diaEHora).innerHTML += "<li onclick='riscar(this)'>"+atividade+"</li>"
         document.getElementById("atividade").value="";
     }
     else{
