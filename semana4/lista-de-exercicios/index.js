663e726ce7ff12ffe2e10e71da76d3772848e912
José Mauro Neto <jmauroneto@gmail.com>
2020-04-10 08:12:04 -0300

    Finalizando exercício 6 de funções de array

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -329,3 +329,22 @@ const email = consultas.map(consulta =>{
     }
     return `Olá, ${tratamento} ${consulta.nome}. Infelizmente, sua consulta marcada para o dia ${consulta.dataDaConsulta} foi cancelada. Se quiser, pode entrar em contato conosco para remarcá-la`
 } )
+
+
+//Exercício 6
+const contas = [
+	{ cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+	{ cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+	{ cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+	{ cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+	{ cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+	{ cliente: "Soter", saldoTotal: 1200, compras: [] }
+]
+
+contas.forEach(conta => {
+    let totalCompras=0;
+    for(let compra of conta.compras){
+        totalCompras+=compra;
+    }
+    conta.saldoTotal -= totalCompras;
+})
