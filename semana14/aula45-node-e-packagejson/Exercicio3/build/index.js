753839428a05459e909b1f7764cb62bca90b0c3b
José Mauro Neto <jmauroneto@gmail.com>
2020-06-23 15:23:09 -0300

    Finalizando exercícios

---
+++ semana14/aula45-node-e-packagejson/Exercicio3/build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs");
+const arquivo = process.argv[2];
+const novaTarefa = process.argv[3] + "\n";
+fs.appendFileSync(arquivo, novaTarefa);
+//# sourceMappingURL=index.js.map
