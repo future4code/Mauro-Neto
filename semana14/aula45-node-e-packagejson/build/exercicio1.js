2879a3ba5905e9c8f22b53a2af070610f2325880
José Mauro Neto <jmauroneto@gmail.com>
2020-06-23 14:20:37 -0300

    Exercicio 1

---
+++ semana14/aula45-node-e-packagejson/build/exercicio1.js
@@ -0,0 +1,5 @@
+const nome = process.argv[2];
+const idade = Number(process.argv[3]);
+console.log(`Olá, ${nome}! Você tem ${idade} anos.`);
+console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${idade + 7}`);
+//# sourceMappingURL=exercicio1.js.map
